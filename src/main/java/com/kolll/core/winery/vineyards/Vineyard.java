7d562ed88ea8b6ec46cbe0b737568678b4cd9edc
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-05-13 11:36:23 +0400

    add methods manage area vineyard

--- src/main/java/com/kolll/core/winery/vineyards/Vineyard.java
+++ src/main/java/com/kolll/core/winery/vineyards/Vineyard.java
@@ -1,4 +1,30 @@
 package com.kolll.core.winery.vineyards;
 
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
 public class Vineyard implements Vineyards {
+    Long area;
+
+    public Vineyard(Long area) {
+        this.area = area;
+    }
+
+    private void addArea (Long difference){
+        area += difference;
+    }
+
+    private void reduceArea (Long difference){
+        area-= difference;
+    }
+
+    public Long getArea() {
+        return area;
+    }
+
+    public void setArea(Long area) {
+        this.area = area;
+    }
 }
