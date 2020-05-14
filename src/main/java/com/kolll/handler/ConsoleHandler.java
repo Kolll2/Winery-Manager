e75efe8c02bedeb48736761e1e201053fb69b5ad
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-05-14 23:04:02 +0400

    Create ConsoleHandler.java

---
+++ src/main/java/com/kolll/handler/ConsoleHandler.java
@@ -0,0 +1,18 @@
+package com.kolll.handler;
+
+import java.util.Scanner;
+
+public class ConsoleHandler {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static String getCommnand (){
+        return scanner.nextLine();
+    }
+
+    public static Long parseLong (String str){
+
+        return 0L;
+    }
+
+}
